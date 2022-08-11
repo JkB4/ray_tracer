649fef9d55a224fd587785947c72122b76a9d304
JkB4 <jakob.bekken@gmail.com>
2022-08-11 15:20:26 +0200

    Completed ray and tested colors

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,46 @@
 mod vec3;
 mod ray;
 
+use ray::Ray;
+use crate::vec3::Vec3;
+
+fn ray_clr(r: &Ray) -> Vec3 {
+    let unit_direction = Vec3::normalize(&r.direction());
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
+}
+
+fn write_clr(px_clr: Vec3) {
+    let r = (255.99 * px_clr.r()) as u32;
+    let g = (255.99 * px_clr.g()) as u32;
+    let b = (255.99 * px_clr.b()) as u32;
+
+    println!("{} {} {}", r, g, b);
+}
+
+
 fn main() {
     let w = 256;
     let h = 256;
     let max_value = 255;
+
+
+    let origin = Vec3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(4.0, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, 2.0, 0.0);
+    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
 
     println!("P3\n{} {}\n{}", w, h, max_value);
 
     for j in (0..h).rev() {
         for i in 0..w {
 
-            let r = i as f32 / w as f32;
-            let g = j as f32 / h as f32;
-            let b = 1 as f32;
+            let u = i as f32 / w as f32;
+            let v = j as f32 / h as f32;
 
-            let ir = (255.99 * r) as u32;
-            let ig = (255.99 * g) as u32;
-            let ib = (255.99 * b) as u32;
+            let r= Ray::new(origin, lower_left_corner + horizontal * u + vertical * v);
 
-            println!("{} {} {}", ir, ig, ib);
+            write_clr(ray_clr(&r));
         }
     }
 }
