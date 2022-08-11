cefaa9fb58dbb8ecfa1077d050d22ad393f29f78
JkB4 <jakob.bekken@gmail.com>
2022-08-11 14:38:27 +0200

    Added ray class and printed quick image

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 mod vec3;
-
-use vec3::Vec3;
+mod ray;
 
 fn main() {
-    let u = Vec3::new(1.0, 2.0, 0.0);
+    let w = 256;
+    let h = 256;
+    let max_value = 255;
 
-    println!("{}", Vec3::normalize(&u));
-    println!("{}", u);
+    println!("P3\n{} {}\n{}", w, h, max_value);
+
+    for j in (0..h).rev() {
+        for i in 0..w {
+
+            let r = i as f32 / w as f32;
+            let g = j as f32 / h as f32;
+            let b = 1 as f32;
+
+            let ir = (255.99 * r) as u32;
+            let ig = (255.99 * g) as u32;
+            let ib = (255.99 * b) as u32;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
 }
---
+++ src/ray.rs
@@ -0,0 +1,27 @@
+use crate::vec3::Vec3;
+
+struct Ray {
+    origin: Vec3,
+    direction: Vec3,
+}
+
+impl Ray {
+    pub fn new(a: Vec3, b: Vec3) -> Ray {
+        Ray {
+            origin: a,
+            direction: b,
+        }
+    }
+
+    pub fn origin(&self) -> Vec3 {
+        self.origin
+    }
+
+    pub fn direction(&self) -> Vec3 {
+        self.direction
+    }
+
+    pub fn at(&self, t: f32) -> Vec3 {
+        self.origin + t * self.direction
+    }
+}
